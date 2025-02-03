09302519806a87926a2acd9ea2a994e2f5ebd00f
memesmith0 <meme.smith.wizard@gmail.com>
2025-02-03 06:58:50 -0800

    Update awk-fastlisp-blc-compiler.sh
    reformatted

--- awk-fastlisp-blc-compiler.sh
+++ awk-fastlisp-blc-compiler.sh
@@ -9,29 +9,29 @@
 #RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 #
 #THIS SOFTWARE.
-#get input file into pipeline
-cat "$1" |
 
     #convert newlines to spaces
-    busybox awk '{ printf "%s ", $0 }' |
+convert_newlines_to_spaces(){ busybox awk '{ printf "%s ", $0 }' ; } ;
 
-    #convert spaces to newlines
-    busybox awk '{ gsub(/ /, "\n"); print }' |
+#convert spaces to newlines
+
+convert_spaces_to_newlines(){ busybox awk '{ gsub(/ /, "\n"); print }' ; } ;
 
     #convert ) to \n)\n
-    busybox awk '{ gsub(/\)/, "\n)\n"); print; }' |
+separate_right_paren(){ busybox awk '{ gsub(/\)/, "\n)\n"); print; }' ; } ;
 
-    #convert ( to \n(\n
-    busybox awk '{ gsub(/\(/, "\n(\n"); print; }' |
+#convert ( to \n(\n
+separate_left_paren(){ busybox awk '{ gsub(/\(/, "\n(\n"); print; }' ; } ;
 
     #delete contents of comment nodes
-    busybox awk '{ printing = 1 } previous_line == "(" && $0 == "comment" { printing = 0, depth = 1 } printing == 0 && $0 == "(" { depth = depth + 1 } printing == 0 && $0 == ")" { depth = depth - 1 } depth == 0 { printing = 1 } printing == 1 { print $0 } { previous_line = $0 }' |
+delete_comments(){ busybox awk '{ printing = 1 } previous_line == "(" && $0 == "comment" { printing = 0, depth = 1 } printing == 0 && $0 == "(" { depth = depth + 1 } printing == 0 && $0 == ")" { depth = depth - 1 } depth == 0 { printing = 1 } printing == 1 { print $0 } { previous_line = $0 }' ; } ;
 
-    #break the file up into characters
-    busybox awk '{for (i = 1; i <= length($0); i++) printf "%c\n", substr($0, i, 1)}{printf "\n"}' |
+#break the file up into characters
+break_file_into_characters(){ busybox awk '{for (i = 1; i <= length($0); i++) printf "%c\n", substr($0, i, 1)}{printf "\n"}' ; } ;
 
-    
-    busybox awk 'BEGIN    { _ord_init() }
+
+
+turn_characters_into_ints(){ busybox awk 'BEGIN    { _ord_init() }
 
 function _ord_init(    low, high, i, t)
 {
@@ -41,7 +41,7 @@ function _ord_init(    low, high, i, t)
         high = 127
     } else if (sprintf("%c", 128 + 7) == "\a") {
         # ascii, mark parity
-        low = 128
+       low = 128
         high = 255
     } else {        # ebcdic(!)
         low = 0
@@ -67,9 +67,9 @@ function chr(c)
 }
 
 {print ord($0)}
-' |
+' ; } ;
 
-busybox awk '{
+turn_ints_into_binary(){ busybox awk '{
   num = $0;
   if (num < 0) {
     print "Negative numbers not supported"; # Or handle them as you wish
@@ -87,44 +87,55 @@ busybox awk '{
     num = int(num / 2); # Integer division is crucial
   }
   print bin;
-}'
+}' ; } ;
+
+prepend_0(){ busybox awk '{
+
+	    for(i=(8-length($0)); i > 0; i--){
+
+	    printf "%s", 0
+	    
+	    }
+	    printf "%s\n", $0
 
 
-#    busybox awk '{if($0 == "00101001"){print "yes"}}'
+
+    }';
 
 
+	   }
 
-#    busybox awk '
-#{
-#
-#	is_closing_paren=( $0 == "00101001" );
-#	if(is_closing_paren){print "yes"};
-#	if($0 == "00101001"){print "yes"};
-#
-#	if(text_mode){
-#
-#		    for(i=0; i<8; i++){
-#				    foo=substr($0, i, i + 1);
-#				    printf "%s%s", "((lambda x (lambda y (lambda z (z x y)))) ", (foo ? " (lambda x (lambda y x)) "  : "(lambda x (lambda y y ))")
-#				    bit_counter++;
-#		    		    };
-#		    }
-#	else if(is_closing_paren){
-#		print "hellohellohelll0348909385093485039485098";
-#		printf "%s%s", "(lambda x (lambda y y))";
-#		for(i=0;bitcounter==0; bitcounter--){
-#				      printf "%s", ")";
-#				      };
-#	        printf("\n");
-#n
-#	}
-#	else {print $0};
-#
-#	{a5=a4; fa4=a3; a3=a2; a2=a1; a1=a0; a0=$0};
-###	if((!is_closing_paren) && a5=="00101000" && a4=="01010100" && a3=="01000101" && a2=="01011000" && a1=="01010100" && a0=="00100000"){text_mode=1;bit_counter=1;print "hello";}
 
-#}
-#'
+convert_lambdas(){ busybox awk '
+{
+
+	is_closing_paren=( $0 == "00101001" );
+	if(is_closing_paren){print "yes"};
+	if($0 == "00101001"){print "yes"};
+
+	if(text_mode){
+
+		    for(i=0; i<8; i++){
+				    foo=substr($0, i, i + 1);
+				    printf "%s%s", "((lambda x (lambda y (lambda z (z x y)))) ", (foo ? " (lambda x (lambda y x)) "  : "(lambda x (lambda y y ))")
+				    bit_counter++;
+		    		    };
+		    }
+	else if(is_closing_paren){
+		printf "%s%s", "(lambda x (lambda y y))";
+		for(i=0;bitcounter==0; bitcounter--){
+				      printf "%s", ")";
+				      };
+	        printf("\n");
+n
+	}
+	else {print $0};
+
+	{a5=a4; fa4=a3; a3=a2; a2=a1; a1=a0; a0=$0};
+	if((!is_closing_paren) && a5=="00101000" && a4=="01010100" && a3=="01000101" && a2=="01011000" && a1=="01010100" && a0=="00100000"){text_mode=1;bit_counter=1;print "hello";}
+
+}
+' ; }
 
 
 #00101000      "("
@@ -133,6 +144,20 @@ busybox awk '{
 #01011000      "X"
 #01010100      "T"
 #00100000      " "
+
+
+
+#get input file into pipeline
+cat "$1" convert_newlines_to_spaces |
+	 convert_spaces_to_newlines | 
+	 separate_right_paren |
+         separate_left_paren |
+	 delete_comments |
+	 break_file_into_characters |
+	 turn_characters_into_ints |
+	 turn_ints_into_binary |
+	 prepend_0 #|
+#	 convert_lambdas
 
     
 
