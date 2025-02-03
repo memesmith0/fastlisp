5051a7f56e793389f54bb79a33c77d6866146c99
memesmith0 <meme.smith.wizard@gmail.com>
2025-02-03 03:16:08 -0800

    Update awk-fastlisp-blc-compiler.sh
    asdofijasdofijasodifj

--- awk-fastlisp-blc-compiler.sh
+++ awk-fastlisp-blc-compiler.sh
@@ -37,7 +37,7 @@ cat "$1" |
     busybox awk '{for (i = 1; i <= length($0); i=i+2) printf "%c%c\n", substr($0, i, 1), substr($0, i+1,1)}' |
 
       #convert hex to binary
-    busybox awk 'function hex2bin(hex){ if(hex=="0"){return "0000"} if(hex=="1"){return "0001"} if(hex=="2"){return "0010"} if(hex=="3"){return "0011"} if(hex=="4"){return "0100"} if(hex=="5"){return "0101"} if(hex=="6"){return "0110"} if(hex=="7"){return "0111"} if(hex=="8"){return "1000"} if(hex=="9"){return "1001"} if(hex=="a"){return "1010"} if(hex=="b"){return "1011"} if(hex=="c"){return "1100"} if(hex=="d"){return "1101"} if(hex=="e"){return "1110"} if(hex=="f"){return "1111"}} {a=substr($0,0,1); b=substr($0,1,1); printf "%s\n%s" hex2bin(a), hex2bin(b)}'
+    busybox awk 'function hex2bin(hex){ if(hex=="0"){return "0000"} if(hex=="1"){return "0001"} if(hex=="2"){return "0010"} if(hex=="3"){return "0011"} if(hex=="4"){return "0100"} if(hex=="5"){return "0101"} if(hex=="6"){return "0110"} if(hex=="7"){return "0111"} if(hex=="8"){return "1000"} if(hex=="9"){return "1001"} if(hex=="a"){return "1010"} if(hex=="b"){return "1011"} if(hex=="c"){return "1100"} if(hex=="d"){return "1101"} if(hex=="e"){return "1110"} if(hex=="f"){return "1111"}} {a=substr($0,0,1); b=substr($0,1,1); printf "%s%s" hex2bin(a), hex2bin(b); print ""}'
 
 
 #
